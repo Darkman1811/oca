be65c2629095d3c4a2209af9a3f7f8422bb5e00c
root <root@guestu.sn+9>
2016-10-30 15:08:15 +0000

    offsetDateTime

--- chap12_DatesAndTimes/test.java
+++ chap12_DatesAndTimes/test.java
@@ -6,13 +6,13 @@ public class test{
 	public static void main(String [] args){
 		System.out.println("Testing Dates: Zone offset");     
         
-        ZoneOffset zof1=ZoneOffset.ofHours(+6);
-        ZoneOffset zof2=ZoneOffset.ofHoursMinutes(+5,30);
-        ZoneOffset zof3=ZoneOffset.ofHoursMinutesSeconds(+1,30,15);
+        Instant i= Instant.now();
+        System.out.println("now hiere: "+i);
+        ZoneOffset zof=ZoneOffset.ofHoursMinutesSeconds(+6,30,15);
+        System.out.println("Zone offset: "+zof);
+		OffsetDateTime odt=i.atOffset(zof);
+		System.out.println("At the Zone offset: "+odt);
 		
-		System.out.println("Zone 1: "+zof1);
-		System.out.println("Zone 2: "+zof2);
-		System.out.println("Zone 3: "+zof3);
-	
+
 	}
 }
