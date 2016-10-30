e7958258a29c32cb207b189c23a8abd780b21545
root <root@guestu.sn+9>
2016-10-30 14:14:40 +0000

    Duration 2

--- chap12_DatesAndTimes/test.java
+++ chap12_DatesAndTimes/test.java
@@ -8,9 +8,11 @@ public class test{
         LocalDateTime ldt1=LocalDateTime.of(1985,11,18,18,35,40,50);
         LocalDateTime ldt2=LocalDateTime.now();
         Duration d1=Duration.between(ldt1,ldt2);
+        // System.out.println("Age:"+d1.dividedBy(365)+" years"); -- Does not work
+        // System.out.println("Age:"+d1.dividedBy(30)+" months"); -- Does not work
         System.out.println("Age:"+d1.toDays()+" jours"); //Converting duration to years, months is not possible
-        System.out.println("Age:"+d1.toHours()+" heurs"); //Converting duration to years, months is not possible
-        System.out.println("Age:"+d1.toMinutes()+" minutes"); //Converting duration to years, months is not possible
+        System.out.println("Age:"+d1.toHours()+" heurs"); 
+        System.out.println("Age:"+d1.toMinutes()+" minutes");
 
         Instant i= Instant.now().plus(d1);
         System.out.println("You will reach the double of your age at: "+i);
