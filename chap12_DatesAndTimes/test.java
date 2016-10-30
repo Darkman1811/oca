1da8d3f006e828010d2c588fa02bd53b6220ade3
root <root@guestu.sn+9>
2016-10-30 08:19:52 +0000

    ofXXX

--- chap12_DatesAndTimes/test.java
+++ chap12_DatesAndTimes/test.java
@@ -3,10 +3,18 @@ import java.time.zone.*;
 public class test{
 
 	public static void main(String [] args){
-		System.out.println("bobo");        
-		LocalDate ld=LocalDate.now();
-		LocalTime lt=LocalTime.now();
-		LocalDateTime ldt=LocalDateTime.now();
+		System.out.println("Testing Dates");     
+
+		LocalDate ld=LocalDate.of(2016,10,29);
+		ld=LocalDate.ofYearDay(1985,18);
+		// ld=LocalDate.ofYearMonth(1985,18); -- Year an Month only does not work
+
+		LocalTime lt=LocalTime.of(07,46,13);
+		lt=LocalTime.of(07,46); //Time without second
+		// lt=LocalTime.of(07); -- Hours only does not work
+		lt=LocalTime.ofSecondOfDay(75000);  //Time from a number of seconds
+
+		LocalDateTime ldt=LocalDateTime.of(2016,10,29,07,46,13);
 		// ZoneDateTime.now();
 		System.out.println("Date: "+ld);
 		System.out.println("Time: "+lt);
