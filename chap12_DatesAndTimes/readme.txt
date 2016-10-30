fc548f8069b28a06756aa4be7116d5d25cdb7065
root <root@guestu.sn+9>
2016-10-30 07:45:08 +0000

    LocalDate, LocalTime, LocalDateTime

--- chap12_DatesAndTimes/readme.txt
+++ chap12_DatesAndTimes/readme.txt
@@ -1,2 +1,5 @@
 Programme:
-   1-
+   1-Principale classes: LocalDate, LocalTime, LocalDateTime, ZoneDateTime
+   2-Prefixes testing: ofXXX, fromXXX, withXXX, getXXX, toXXX, atXXX, plus, minus,multipliedBy, devidedBy, negated
+   3-Instants & Durations
+   4-ZoneOffset & ZoneId
--- chap12_DatesAndTimes/test.java
+++ chap12_DatesAndTimes/test.java
@@ -1,8 +1,15 @@
 import java.time.*;
+import java.time.zone.*;
 public class test{
 
 	public static void main(String [] args){
-		System.out.println("bobo");
-
+		System.out.println("bobo");        
+		LocalDate ld=LocalDate.now();
+		LocalTime lt=LocalTime.now();
+		LocalDateTime ldt=LocalDateTime.now();
+		// ZoneDateTime.now();
+		System.out.println("Date: "+ld);
+		System.out.println("Time: "+lt);
+		System.out.println("DateTilme: "+ldt);
 	}
 }
