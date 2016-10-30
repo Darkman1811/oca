961109aaace18059af9c5a27986d7435d73ac34b
root <root@guestu.sn+9>
2016-10-30 16:14:20 +0000

    zoneids like <<Africa/Dakar>>

--- chap12_DatesAndTimes/test.java
+++ chap12_DatesAndTimes/test.java
@@ -1,14 +1,28 @@
 import java.time.*;
-import java.util.Set;
+import java.time.temporal.*;
+import static java.time.temporal.TemporalQueries.zone;
+
 public class test{
 
 	public static void main(String [] args){
 		System.out.println("Testing Dates: Zone offset");     
         
-        Set<String> zoneIds= ZoneId.getAvailableZoneIds();
-        for(String zoneid:zoneIds){
-        	System.out.println(zoneid);
-        }
+        Instant i= Instant.now();
+        System.out.println("now hiere: "+i);
+
+
+        ZoneId zoneDakar=ZoneId.of("Africa/Dakar");
+		ZonedDateTime atDakar=i.atZone(zoneDakar);
+		System.out.println("At the Zone offset: "+atDakar);
+
+
+        ZoneId zoneTokyo=ZoneId.of("Asia/Tokyo");
+		ZonedDateTime atTokyo=i.atZone(zoneTokyo);
+		System.out.println("At the Zone offset: "+atTokyo);
+
+        ZoneId zoneMonaco=ZoneId.of("Europe/Monaco");
+		ZonedDateTime atMonaco=i.atZone(zoneMonaco);
+		System.out.println("At the Zone offset: "+atMonaco);
 		
 
 	}
