4c9dfb879647d23132cf7a51914ed4c9c32f1edf
root <root@guestu.sn+9>
2016-10-30 13:48:25 +0000

    Duration

--- chap12_DatesAndTimes/test.java
+++ chap12_DatesAndTimes/test.java
@@ -5,28 +5,16 @@ public class test{
 
 	public static void main(String [] args){
 		System.out.println("Testing Dates");     
+        LocalDateTime ldt=LocalDateTime.of(2016,11,18,18,35,40,50);
+		Instant i=Instant.ofEpochSecond(ldt.toEpochSecond(ZoneOffset.UTC));
+		System.out.println("origin: "+i);
 
-		Instant i=Instant.now();
-		System.out.println("Instant now: "+i);
-        i=i.plus(3,ChronoUnit.DAYS);
-        System.out.println("+3 days: "+i);
+		Duration du=Duration.ofMinutes(15);
+		System.out.println("Dureé de 15mn: "+du);
 
-        i=i.plus(1,ChronoUnit.HALF_DAYS);
-        System.out.println("+1 half day: "+i);
+		i=i.plus(du);
+		System.out.println("Instant + duréé 25 mn "+i);
 
-        i=i.plus(5,ChronoUnit.HOURS);
-        System.out.println("+5 hours: "+i);
-
-        i=i.plus(15,ChronoUnit.MINUTES);
-        System.out.println("+15 minutes: "+i);
-
-        i=i.plus(41,ChronoUnit.SECONDS);
-        System.out.println("+41 seconds: "+i);
-
-        i=i.plus(56,ChronoUnit.NANOS);
-        System.out.println("+56 nanoseconds: "+i);
-
-        
-		
+        		
 	}
 }
