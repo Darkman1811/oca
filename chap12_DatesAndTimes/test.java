8ea5d5636fdba1ccab9ca009bc6f6753371d0210
root <root@guestu.sn+9>
2016-10-30 12:13:50 +0000

    plusXXX

--- chap12_DatesAndTimes/test.java
+++ chap12_DatesAndTimes/test.java
@@ -7,21 +7,50 @@ public class test{
 
 		
 		LocalDateTime ldt=LocalDateTime.now();
-
 		LocalDate ld=LocalDate.now();
-		ldt=ld.atStartOfDay();
-		System.out.println("LocalDateTime: "+ldt);
-        
-        ldt=ld.atTime(15,30);
-        System.out.println("LocalDateTime: "+ldt);
-
-        ld=Year.of(2017).atMonth(5).atDay(22);        
-        System.out.println("LocalDate: "+ld);
-        
         LocalTime lt=LocalTime.now();
-        ldt=lt.atDate(ld);
-        System.out.println("LocalDateTime: "+ldt);
 
+        System.out.println("LocalDate:");
+		System.out.println("Today: "+ld);
+        ld=ld.plusYears(3);
+        System.out.println("+3 years: "+ld);
+        ld=ld.plusMonths(4);
+        System.out.println("+4 months: "+ld);
+        ld=ld.plusWeeks(2);
+        System.out.println("+ two weeks: "+ld);
+        ld=ld.plusDays(9);
+        System.out.println("plus 9 days: "+ld);
+
+        System.out.println("\n \n LocalTime:");        
+        System.out.println("now: "+lt);
+        lt=lt.plusHours(4);
+        System.out.println("+4 Hours: "+lt);
+        lt=lt.plusMinutes(25);
+        System.out.println("+25 minutes: "+lt);
+        lt=lt.plusSeconds(30);
+        System.out.println("+30 secondes: "+lt);
+        lt=lt.plusNanos(17);
+        System.out.println("+17 nanosecodes: "+lt);
+
+        System.out.println("\n \n LocalDateTime:");
+        System.out.println("Today now: "+ldt);
+        ldt=ldt.plusYears(3);
+        System.out.println("+3 years: "+ldt);
+        ldt=ldt.plusMonths(4);
+        System.out.println("+4 months: "+ldt);
+        ldt=ldt.plusWeeks(2);
+        System.out.println("+ two weeks: "+ldt);
+        ldt=ldt.plusDays(9);
+        System.out.println("plus 9 days: "+ldt);
+        ldt=ldt.plusHours(4);
+        System.out.println("+4 Hours: "+ldt);
+        ldt=ldt.plusMinutes(25);
+        System.out.println("+25 minutes: "+ldt);
+        ldt=ldt.plusSeconds(30);
+        System.out.println("+30 secondes: "+ldt);
+        ldt=ldt.plusNanos(17);
+        System.out.println("+17 nanosecodes: "+ldt);
+        
 		
 	}
 }
