fe9ef277e481a306e3404131013a4123ee4baa3d
root <root@guestu.sn+9>
2016-10-30 11:24:27 +0000

    toXXX

--- chap12_DatesAndTimes/test.java
+++ chap12_DatesAndTimes/test.java
@@ -7,25 +7,14 @@ public class test{
 
 		
 		LocalDateTime ldt=LocalDateTime.now();
-		LocalDate ld=LocalDate.now();
-		int y=ld.getYear();
-		Month m=ld.getMonth();
-		int d=ld.getDayOfMonth();
+		LocalDate ld=ldt.toLocalDate();
+		
+		LocalTime lt=ldt.toLocalTime();
+        
+		System.out.println("LocalDateTime: "+ldt);
+		System.out.println("LocalDate: "+ld);
+		System.out.println("LocalTime: "+lt);
 
-		LocalTime lt=LocalTime.now();
-        int h=lt.getHour();
-        int min=lt.getMinute();
-        int s=lt.getSecond();
-        int n=lt.getNano();
-
-
-		System.out.println("Year: "+y);
-		System.out.println("Month: "+m);
-		System.out.println("Day: "+d);
-
-		System.out.println("Hours: "+h);
-		System.out.println("Month: "+min);
-		System.out.println("Minutes: "+s);
-		System.out.println("NanoSeconds: "+n);
+		
 	}
 }
