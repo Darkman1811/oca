7b806a35b8f89a8c54ce9b6caff867daa4554880
root <root@guestu.sn+9>
2016-10-30 15:19:12 +0000

    zoneIds list

--- chap12_DatesAndTimes/test.java
+++ chap12_DatesAndTimes/test.java
@@ -1,17 +1,14 @@
 import java.time.*;
-import java.time.temporal.*;
-
+import java.util.Set;
 public class test{
 
 	public static void main(String [] args){
 		System.out.println("Testing Dates: Zone offset");     
         
-        Instant i= Instant.now();
-        System.out.println("now hiere: "+i);
-        ZoneOffset zof=ZoneOffset.ofHoursMinutesSeconds(+6,30,15);
-        System.out.println("Zone offset: "+zof);
-		OffsetDateTime odt=i.atOffset(zof);
-		System.out.println("At the Zone offset: "+odt);
+        Set<String> zoneIds= ZoneId.getAvailableZoneIds();
+        for(String zoneid:zoneIds){
+        	System.out.println(zoneid);
+        }
 		
 
 	}
