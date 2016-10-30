59164ecd5056fe04585e2b2fa1f2bafe162ac83d
root <root@guestu.sn+9>
2016-10-30 14:37:09 +0000

    zoneoffset1

--- chap12_DatesAndTimes/test.java
+++ chap12_DatesAndTimes/test.java
@@ -4,19 +4,15 @@ import java.time.temporal.*;
 public class test{
 
 	public static void main(String [] args){
-		System.out.println("Testing Dates: Duration");     
-        LocalDateTime ldt1=LocalDateTime.of(1985,11,18,18,35,40,50);
-        LocalDateTime ldt2=LocalDateTime.now();
-        Duration d1=Duration.between(ldt1,ldt2);
-        // System.out.println("Age:"+d1.dividedBy(365)+" years"); -- Does not work
-        // System.out.println("Age:"+d1.dividedBy(30)+" months"); -- Does not work
-        System.out.println("Age:"+d1.toDays()+" jours"); //Converting duration to years, months is not possible
-        System.out.println("Age:"+d1.toHours()+" heurs"); 
-        System.out.println("Age:"+d1.toMinutes()+" minutes");
-
-        Instant i= Instant.now().plus(d1);
-        System.out.println("You will reach the double of your age at: "+i);
+		System.out.println("Testing Dates: Zone offset");     
+        
+        ZoneOffset zof1=ZoneOffset.ofHours(+6);
+        ZoneOffset zof2=ZoneOffset.ofHoursMinutes(+5,30);
+        ZoneOffset zof3=ZoneOffset.ofHoursMinutesSeconds(+1,30,15);
 		
+		System.out.println("Zone 1: "+zof1);
+		System.out.println("Zone 2: "+zof2);
+		System.out.println("Zone 3: "+zof3);
 	
 	}
 }
