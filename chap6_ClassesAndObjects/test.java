84a77d294d28fde972c078925c0508c031b93844
root <root@guestu.sn+9>
2016-11-01 12:33:53 +0000

    constructor overloading

--- chap6_ClassesAndObjects/test.java
+++ chap6_ClassesAndObjects/test.java
@@ -3,12 +3,16 @@ public class test{
 	public static void main(String [] args){
 		System.out.println("bobo");
         System.out.println(new test().song);
+        System.out.println(new test("custom").song);
        
 	}
 
-/*	public test(){       //commenting this methode out will work but song's value will be instance
+	public test(){       //commenting this methode out will work but song's value will be instance
 		song="default";
 	}
- */   
-
+    
+    public test(String song){       
+		this.song=song;
+		this.song=song;
+	}
 }
