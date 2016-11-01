cc38d258adb327bb693bf7b70b3cc8095f604b04
root <root@guestu.sn+9>
2016-11-01 12:42:32 +0000

    instanciating classes

--- chap6_ClassesAndObjects/test.java
+++ chap6_ClassesAndObjects/test.java
@@ -2,21 +2,9 @@ public class test{
 		String song="instance";  
 	public static void main(String [] args){
 		System.out.println("bobo");
-        System.out.println(new test().song);
-      //  System.out.println(new test("custom").song);
+       test t=new test();
+        System.out.println(t.song);
        
 	}
 
-	public test(){       //commenting this methode out will work but song's value will be instance
-		this("custom1"); //this must be the first sentence of the constructor
-
-		//System.out.println(test("custom1"));  //-- does not work
-		//                                      //-- you cannot either call new from the constructor
-
-	}
-    
-    public test(String song){       
-		this.song=song;
-		this.song=song;
-	}
 }
