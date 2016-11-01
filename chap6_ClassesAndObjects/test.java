10ae7d22cf0c83a29fbe5a2a44b757698b53cd5a
root <root@guestu.sn+9>
2016-11-01 14:59:11 +0000

    byreference & byvalue

--- chap6_ClassesAndObjects/test.java
+++ chap6_ClassesAndObjects/test.java
@@ -1,12 +1,29 @@
-import static pkg1.Personne.espece;
 public class test{
 	    
 		
 	public static void main(String [] args){
 		System.out.println("bobo");
-       System.out.println(espece);
+		test t= new test();
+		int i=5; System.out.println("before: "+i);
+        t.byvalue(i);
+        System.out.println("after: "+i);
+
+        StringBuilder sb= new StringBuilder("Hello"); System.out.println("before: "+sb);
+        t.byreference(sb);
+        System.out.println("after: "+sb);
+
        
 	}
 
+   private void byvalue(int i){
+   	i++;
+   	System.out.println("during: "+i);
+
+   }
+
+   private void byreference(StringBuilder sb){
+   	sb.append(" Abdoulaye");
+   	System.out.println("during: "+sb);
+   }
 }
 
