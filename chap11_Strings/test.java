3143f63740dbfd029f907e14f5238bee917b6e3e
root <root@guestu.sn+9>
2016-11-01 06:48:39 +0000

    length

--- chap11_Strings/test.java
+++ chap11_Strings/test.java
@@ -1,7 +1,11 @@
 public class test{
 	public static void main(String [] args){
 		System.out.println("bobo");
+//------------------------------------------------------------------------------INSTANCIATION
 		String str1= "Hello";
 		String str2= new String ("Hello");
+//------------------------------------------------------------------------------FUNCTIONS
+		int lenght= str1.length(); System.out.println("lenght of str1: "+lenght);
+
 	}
 }
