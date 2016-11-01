c42a6a5181647a5433b0d060dd1102312e277f15
root <root@guestu.sn+9>
2016-11-01 09:07:57 +0000

    length

--- chap15_Arrays/test.java
+++ chap15_Arrays/test.java
@@ -2,13 +2,10 @@ public class test{
 	public static void main(String [] args){
 		System.out.println("Arrays");
 		int [] arr1= {1,2,3,4,5,6,7,8}; 
+		int len1=arr1.length;System.out.println(len1);
 		
 		String []arr2={"Abdoulaye","Yacine","Fatou"};  
-
-		int i=arr1[3]; System.out.println(i);
-
-		String str=arr2[1]; System.out.println(str);
-		Object obj=arr2[0]; //can be cast 
-
+        int len2=arr2.length; System.out.println(len2);
+		
 	}
 }
