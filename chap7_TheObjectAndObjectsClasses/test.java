079f1c80e9094db5efbb4bc9e6740f53ed97bd13
root <root@guestu.sn+9>
2016-11-01 21:38:24 +0000

    immutable objects

--- chap7_TheObjectAndObjectsClasses/test.java
+++ chap7_TheObjectAndObjectsClasses/test.java
@@ -1,24 +1,39 @@
 public class test{
 	public static void main(String [] args){
 		System.out.println("bobo");
-	    Personne pers= new Personne();
-	    pers.id=1;
-	    pers.name="Abdoulaye";
+        final Personneinternal pi=new Personneinternal(); // No way to update since the members are final too
+        System.out.println(pi.id);   
+        System.out.println(pi.name);
 
-	    Personne pers2=pers.clone();
-	    System.out.println(pers2.id);
-	    System.out.println(pers2.name);
+        final Personneexternal pe= new Personneexternal(1,new StringBuilder("Abdoulaye"));
+	    System.out.println(pe.getId());   
+        System.out.println(pe.getName());
+
+        StringBuilder sb=pe.getName();
+        sb.append("Diaw");
+        System.out.println(pe.getId());   
+        System.out.println(pe.getName());
 	}
 
 }
 
-class Personne{
-	int id;
-	String name;
-public Personne clone(){
-	Personne other=new Personne();
-	other.id=this.id;
-	other.name=this.name;
-	return other;
+class Personneinternal{
+	public final int id=5;
+	public final StringBuilder name=new StringBuilder("Abdoulaye");
 }
+
+class Personneexternal{
+	private int id;
+	private  StringBuilder name;
+    public int getId(){return this.id;}
+   // public void setId(int id){this.id=id ;} //-- make it externally immutable
+
+    public StringBuilder getName(){return this.name;}
+   // public void setName(StringBuilder name){this.name=name;} //-- make it externally immutable
+
+    public Personneexternal(int id,StringBuilder name){
+    	this.id=id;
+    	this.name=name;
+    }
+
 }
