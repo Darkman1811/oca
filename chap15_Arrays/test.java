5b3858b4fcd1ea0e4c3b5827908bfe1c6150710c
root <root@guestu.sn+9>
2016-11-01 09:56:13 +0000

    Arrays as function parameters

--- chap15_Arrays/test.java
+++ chap15_Arrays/test.java
@@ -2,18 +2,35 @@ public class test{
 	public static void main(String [] args){
 		System.out.println("Arrays");
 		
-		String []arr1={"Abdoulaye","Yacine","Fatou"};  
+		String []arr1={"Abdoulaye","Yacine","Fatou","Modou","Moussa"};  
+        
+        System.out.println("Befor workon");
+        for(String str:arr1){
+        	System.out.println(str);
+        }
 
-		String []arr2={"Modou","Moussa"};  
+		String [] arr2= new test().workon(arr1);
 
-		String [] arr3= new String[6];
-         
-        System.arraycopy(arr1,0,arr3,0,3);
-        System.arraycopy(arr2,0,arr3,3,2);
-
-        for(String str:arr3){
+        System.out.println("\n After workon");
+        for(String str:arr2){
         	System.out.println(str);
         }
 		
+	}
+
+	private String[] workon(String [] arr){
+		int len=arr.length; 
+       String [] tmparr=new String[len];
+       System.arraycopy(arr,0,tmparr,0,len);
+
+       int j=len; // compteur de arr 
+       
+       for(int i=0;i<len;i++){//boucle inverse pour tmparr
+       	j--; //incrementer le compteur de i
+       	arr[i]=tmparr[j];
+        }
+
+       
+     return arr;
 	}
 }
