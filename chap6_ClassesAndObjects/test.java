e29f4f2d242f4425c089bed48c5b09a3417af7eb
root <root@guestu.sn+9>
2016-11-01 14:46:11 +0000

    static imports

--- chap6_ClassesAndObjects/test.java
+++ chap6_ClassesAndObjects/test.java
@@ -1,15 +1,12 @@
-import java.util.Date;
-import java.sql.*; //java.util.Date is more specifique so it will take the relevance
+import static pkg1.Personne.espece;
 public class test{
 	    
 		
 	public static void main(String [] args){
 		System.out.println("bobo");
-        Date d=new Date();
-        String str=d.getClass().toString();
-        System.out.println(str);
-
+       System.out.println(espece);
        
 	}
 
 }
+
