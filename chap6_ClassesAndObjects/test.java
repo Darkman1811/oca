eb0755622d13a958c534d39794c93a7ee32b569e
root <root@guestu.sn+9>
2016-11-01 15:54:18 +0000

    varargs

--- chap6_ClassesAndObjects/test.java
+++ chap6_ClassesAndObjects/test.java
@@ -3,38 +3,44 @@ public class test{
 	    static final int f4=15;
 		
 	public static void main(String [] args){
-		final int f1=10;
-		//f1++; //-- not possible as f1 is declared final
-		//new test().f2=12;//-- not possible as f2 is declared final
-        int i=13;
-        new test().workon(i);
-        // f4=16; //-- not possible as f4 is declared final
-
-        final Personne p= new Personne(); 
-        System.out.println(p.getI());
-        p.setI(21);
-        System.out.println(p.getI()); //p is mutable but not his member i;
-      //  p=new Personne(); //- does not wor as p is declared final
+		new test().varargm1(12);
+		new test().varargm1(12,2,4,6); //the third ovverid would make this call ambigious
+		new test().varargm1(12,"hello",4,6);
 	}
 
-  public void workon(final int f3){
-  	//f3=14; //-- not possible as f3 is declared final
-  }
-
-
-}
-
-class Personne{
-	int i;
-    public Personne(){
-    	i=20;
+  public void varargm1(int i1,int...i){
+  	System.out.println("mandatory: "+i1);
+  	int len=i.length;
+  	System.out.println("optional: ");
+  	for(int j=0;j<len;j++){
+  	System.out.println(i[j]);
     }
-	public int getI(){
-		return i;
-	}
+  }
+  
+  public void varargm1(int i1,String str,int...i){
+  	System.out.println("mandatory v2 : ");
+  	System.out.println(i1);
+  	System.out.println(str);
+  	int len=i.length;
+  	System.out.println("optionalv2: ");
+  	for(int j=0;j<len;j++){
+  	System.out.println(i[j]);
+    }
+  }
+/*
+   public void varargm1(int i1,int i2,int...i){
+  	System.out.println("mandatory v3 : ");
+  	System.out.println(i1);
+  	System.out.println(i2);
+  	int len=i.length;
+  	System.out.println("optionalv3: ");
+  	for(int j=0;j<len;j++){
+  	System.out.println(i[j]);
+    }
+  }
+  */
 
-	public void setI(int i){
-		this.i=i;
-	}
 }
+
+
 
