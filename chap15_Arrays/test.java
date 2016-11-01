4404be7ec01db73747789b1813cc1d915f4d85a1
root <root@guestu.sn+9>
2016-11-01 09:24:05 +0000

    system.arraycopy

--- chap15_Arrays/test.java
+++ chap15_Arrays/test.java
@@ -1,21 +1,17 @@
 public class test{
 	public static void main(String [] args){
 		System.out.println("Arrays");
-		int i=0;
-
-		int [] arr1= {1,2,3,4,5,6,7,8}; 
-		int len1=arr1.length;System.out.println(len1);
-		while(i<len1){
-			System.out.println(arr1[i]);
-			i++;
-		}
-        for(int j=0;j<len1-1;j++){
-        	System.out.println(arr1[j]);
-        }
 		
+		String []arr1={"Abdoulaye","Yacine","Fatou"};  
 
-		String []arr2={"Abdoulaye","Yacine","Fatou"};  
-        for(String str:arr2){
+		String []arr2={"Modou","Moussa"};  
+
+		String [] arr3= new String[6];
+         
+        System.arraycopy(arr1,0,arr3,0,3);
+        System.arraycopy(arr2,0,arr3,3,2);
+
+        for(String str:arr3){
         	System.out.println(str);
         }
 		
