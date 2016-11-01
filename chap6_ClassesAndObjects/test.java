b896fbe9cfdac69da6f649a35e2e19166dd745df
root <root@guestu.sn+9>
2016-11-01 12:40:36 +0000

    calling another constructor

--- chap6_ClassesAndObjects/test.java
+++ chap6_ClassesAndObjects/test.java
@@ -3,12 +3,16 @@ public class test{
 	public static void main(String [] args){
 		System.out.println("bobo");
         System.out.println(new test().song);
-        System.out.println(new test("custom").song);
+      //  System.out.println(new test("custom").song);
        
 	}
 
 	public test(){       //commenting this methode out will work but song's value will be instance
-		song="default";
+		this("custom1"); //this must be the first sentence of the constructor
+
+		//System.out.println(test("custom1"));  //-- does not work
+		//                                      //-- you cannot either call new from the constructor
+
 	}
     
     public test(String song){       
