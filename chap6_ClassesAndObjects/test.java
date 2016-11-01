c041eee1007726d38ef59f5c62aaa3fa7db380ed
root <root@guestu.sn+9>
2016-11-01 12:32:02 +0000

    constructors

--- chap6_ClassesAndObjects/test.java
+++ chap6_ClassesAndObjects/test.java
@@ -1,19 +1,14 @@
 public class test{
-	static String song="static"; //instance and classe variables can't have the same name
-//	String song="instance";  //can't access instance variable from static context
+		String song="instance";  
 	public static void main(String [] args){
 		System.out.println("bobo");
-        String song="local1";   //Local variable overide instance & classe variables
-        System.out.println(song);
-        new test().sing(song);
+        System.out.println(new test().song);
+       
 	}
 
-	private void sing(String song){
-       System.out.println(song);
-    //   String song="local2"; //local variables and parameters can't have the same name
-       System.out.println(song);
+	public test(){       //commenting this methode out will work but song's value will be instance
+		song="default";
 	}
-
-
+    
 
 }
