8315d2c2f07606952dbc386f3da9abbc518039b2
root <root@guestu.sn+9>
2016-11-01 21:20:30 +0000

    hashcode & equals

--- chap7_TheObjectAndObjectsClasses/test.java
+++ chap7_TheObjectAndObjectsClasses/test.java
@@ -1,15 +1,35 @@
 public class test{
 	public static void main(String [] args){
 		System.out.println("bobo");
-		String str=new String("Hello");
-		Object obj=str;
-
-		str=(String)str; System.out.println(str);
-
-        System.out.println("obj: "+obj.getClass().getSimpleName());
-		System.out.println("str: "+str.getClass().getSimpleName());
-
-
+	    Personne pers= new Personne();
+	    pers.id=1;
+	    pers.name="Abdoulaye";
+	    System.out.println(pers.hashCode());
+	   
+	    Personne pers2=new Personne();
+        pers2.id=1;
+        pers2.name="Yacine";
+	   System.out.println(pers.equals(pers2));
 	}
+
+}
+
+class Personne{
+	int id;
+	String name;
+public boolean equals(Object obj){
+	 if(obj==this){return true;} 
+	if(!(obj instanceof Personne)){return false;}
+    Personne other=(Personne)obj;
+    if(this.id==other.id){return true;}
+return false;
+}
+
+public int hashCode(){
+  
+int code=18;
+int hashcode=this.id*code;
+return hashcode;
+}
 
 }
