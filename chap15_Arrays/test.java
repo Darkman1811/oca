db04d805de54729426daf593b14d2b89bb6a7ab7
root <root@guestu.sn+9>
2016-11-01 10:10:54 +0000

    initialasing multidimentional arrays

--- chap15_Arrays/test.java
+++ chap15_Arrays/test.java
@@ -2,19 +2,9 @@ public class test{
 	public static void main(String [] args){
 		System.out.println("Arrays");
 		
-		String []arr1={"Abdoulaye","Yacine","Fatou","Modou","Moussa"};  
+		String [][]arr1={{"1","Abdoulaye"},{"2","Yacine"},{"3","Fatou"},{"4","Modou"},{"5","Moussa"}};  
         
-        System.out.println("arr1");
-        for(String str:arr1){
-        	System.out.println(str);
-        }
-        String [] arr2=arr1.clone();
-		
-        System.out.println("\n arr2");
-        for(String str:arr2){
-        	System.out.println(str);
-        }
-		
+      	
 	}
 
 }
