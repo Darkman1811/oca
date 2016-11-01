3cf2474853733683faf2a5608362a4f2a3127dcc
root <root@guestu.sn+9>
2016-11-01 19:57:17 +0000

    instanceof

--- chap7_TheObjectAndObjectsClasses/test.java
+++ chap7_TheObjectAndObjectsClasses/test.java
@@ -1,7 +1,12 @@
 public class test{
 	public static void main(String [] args){
 		System.out.println("bobo");
-		String str=new String("bobo");
+		String str=new String("Hello");
 		Object obj=str;
+
+		str=(String)str; System.out.println(str);
+
+		boolean isString=obj instanceof String; System.out.println("isString: "+isString);
+
 	}
 }
