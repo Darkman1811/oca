b714bd0d662d9529192fb244e7be8e8a1171af49
root <root@guestu.sn+9>
2016-11-01 15:35:16 +0000

    final & runtime mutability

--- chap6_ClassesAndObjects/test.java
+++ chap6_ClassesAndObjects/test.java
@@ -1,29 +1,40 @@
 public class test{
-	    
+	    final int f2=11;
+	    static final int f4=15;
 		
 	public static void main(String [] args){
-		System.out.println("bobo");
-		test t= new test();
-		int i=5; System.out.println("before: "+i);
-        t.byvalue(i);
-        System.out.println("after: "+i);
+		final int f1=10;
+		//f1++; //-- not possible as f1 is declared final
+		//new test().f2=12;//-- not possible as f2 is declared final
+        int i=13;
+        new test().workon(i);
+        // f4=16; //-- not possible as f4 is declared final
 
-        StringBuilder sb= new StringBuilder("Hello"); System.out.println("before: "+sb);
-        t.byreference(sb);
-        System.out.println("after: "+sb);
-
-       
+        final Personne p= new Personne(); 
+        System.out.println(p.getI());
+        p.setI(21);
+        System.out.println(p.getI()); //p is mutable but not his member i;
+      //  p=new Personne(); //- does not wor as p is declared final
 	}
 
-   private void byvalue(int i){
-   	i++;
-   	System.out.println("during: "+i);
+  public void workon(final int f3){
+  	//f3=14; //-- not possible as f3 is declared final
+  }
 
-   }
 
-   private void byreference(StringBuilder sb){
-   	sb.append(" Abdoulaye");
-   	System.out.println("during: "+sb);
-   }
+}
+
+class Personne{
+	int i;
+    public Personne(){
+    	i=20;
+    }
+	public int getI(){
+		return i;
+	}
+
+	public void setI(int i){
+		this.i=i;
+	}
 }
 
