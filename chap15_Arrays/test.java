437548454b4d1a0031709f26860a25a64c589ea4
root <root@guestu.sn+9>
2016-11-01 09:13:55 +0000

    looping arrays

--- chap15_Arrays/test.java
+++ chap15_Arrays/test.java
@@ -1,11 +1,23 @@
 public class test{
 	public static void main(String [] args){
 		System.out.println("Arrays");
+		int i=0;
+
 		int [] arr1= {1,2,3,4,5,6,7,8}; 
 		int len1=arr1.length;System.out.println(len1);
+		while(i<len1){
+			System.out.println(arr1[i]);
+			i++;
+		}
+        for(int j=0;j<len1-1;j++){
+        	System.out.println(arr1[j]);
+        }
 		
+
 		String []arr2={"Abdoulaye","Yacine","Fatou"};  
-        int len2=arr2.length; System.out.println(len2);
+        for(String str:arr2){
+        	System.out.println(str);
+        }
 		
 	}
 }
