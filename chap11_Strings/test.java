d2077dc3b5bc8988257be76a3658cd638b86c978
root <root@guestu.sn+9>
2016-11-01 07:42:21 +0000

    stringbuilder instanciation

--- chap11_Strings/test.java
+++ chap11_Strings/test.java
@@ -1,7 +1,7 @@
 public class test{
 	public static void main(String [] args){
 		System.out.println("bobo");
-		String str1= "Hello";
-		String str2= new String ("Hello");
+		StringBuilder sb1=new StringBuilder("here is the content"); System.out.println(sb1);
+		StringBuilder sb2=new StringBuilder(200); //With size 
 	}
 }
