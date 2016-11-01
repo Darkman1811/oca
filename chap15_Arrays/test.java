9092db564933b117237feeba8a992d2cbe768343
root <root@guestu.sn+9>
2016-11-01 10:11:22 +0000

    accessing multidimentional arrays

--- chap15_Arrays/test.java
+++ chap15_Arrays/test.java
@@ -4,7 +4,16 @@ public class test{
 		
 		String [][]arr1={{"1","Abdoulaye"},{"2","Yacine"},{"3","Fatou"},{"4","Modou"},{"5","Moussa"}};  
         
-      	
+        for(String [] dim1:arr1){
+
+        	// String i=dim1[0]; System.out.println("i: "+i);
+        	// String str=dim1[1]; System.out.println("str: "+str);
+
+        	 for(String  str2:dim1){
+              System.out.println("str: "+str2);
+        	 }
+        
+        }	
 	}
 
 }
