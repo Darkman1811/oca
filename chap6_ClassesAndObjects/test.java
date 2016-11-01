0c696c56e8fd6280e5f8bf3de90dda96daa7e21f
root <root@guestu.sn+9>
2016-11-01 14:26:13 +0000

    wildcard imports

--- chap6_ClassesAndObjects/test.java
+++ chap6_ClassesAndObjects/test.java
@@ -1,28 +1,16 @@
+
+import java.time.*; //does not concerne the subpackages of java.time
+//import java.time.temporal.*; //importing chronofiel from java.time.temporal.ChronoField
 public class test{
-	    boolean b;
-	    byte bt;
-        char c;
-        short sh;
-        int i;
-        long l;
-        float fl;
-        double db;
-		String s;  
+	    
 		
 	public static void main(String [] args){
 		System.out.println("bobo");
-       test t;
-      // System.out.println(t); does not work. t does not even point to null. it does point to anyting
-       t=new test();
-       System.out.println("boolean: "+t.b);
-       System.out.println("byte: "+t.bt);
-       System.out.println("char: "+t.c);
-       System.out.println("short: "+t.sh);
-       System.out.println("int: "+t.i);
-       System.out.println("long: "+t.l);
-       System.out.println("float: "+t.fl);
-       System.out.println("double: "+t.db);
-       System.out.println("String: "+t.s);
+       
+
+        LocalDate ld= LocalDate.now(); System.out.println(ld);
+        System.out.println(ld.get(ChronoField.YEAR)); //not in java.time
+
        
 	}
 
